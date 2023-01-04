1af07c9756fc6e5a075c27e77c6e245d63503d51
Irfan Zidni <irfanzidniofficial@gmail.com>
2023-01-04 22:37:31 +0700

    nearby page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hotel_booking_apps/config/app_asset.dart';
+import 'package:flutter_hotel_booking_apps/config/app_color.dart';
+import 'package:flutter_hotel_booking_apps/controller/c_home.dart';
+import 'package:flutter_hotel_booking_apps/page/nearby_page.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+  final cHome = Get.put(CHome());
+  final List<Map> listNav = [
+    {'icon': AppAsset.iconNearby, 'label': 'Nearby'},
+    {'icon': AppAsset.iconHistory, 'label': 'History'},
+    {'icon': AppAsset.iconPayment, 'label': 'Payment'},
+    {'icon': AppAsset.iconReward, 'label': 'Reward'},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Obx(() {
+        if (cHome.indexPage == 1) {
+          return const Center(child: Text("Index 1"));
+        }
+        return NearbyPage();
+      }),
+      bottomNavigationBar: Obx(() {
+        return Material(
+          elevation: 8,
+          child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.only(top: 8, bottom: 6),
+            child: BottomNavigationBar(
+              currentIndex: cHome.indexPage,
+              onTap: (value) => cHome.indexPage = value,
+              backgroundColor: Colors.transparent,
+              type: BottomNavigationBarType.fixed,
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Colors.black,
+              selectedIconTheme: const IconThemeData(color: AppColor.primary),
+              elevation: 0,
+              selectedFontSize: 12,
+              items: listNav.map((e) {
+                return BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage(e['icon'])),
+                  label: e['label'],
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      }),
+    );
   }
 }
